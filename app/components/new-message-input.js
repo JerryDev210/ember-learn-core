da22f236a5c465a3d563a14fd40bd978cd49d272
Ramkumar G <ramkumar213supm@gmail.com>
2026-01-28 10:08:20 +0530

    Binding Input and Sending Message

--- app/components/new-message-input.js
+++ app/components/new-message-input.js
@@ -1,3 +1,23 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default class NewMessageInput extends Component {}
+export default class NewMessageInput extends Component {
+  @tracked message = '';
+
+  @action
+  updateMessage(event) {
+    let formData = new FormData(event.currentTarget);
+    this.message = formData.get('message');
+  }
+  @action
+  createMessage(event) {
+    event.preventDefault();
+
+    if (this.message && this.args.onCreate) {
+      this.args.onCreate(this.message);
+
+      this.message = '';
+    }
+  }
+}
--- app/components/received-message/index.js
+++ app/components/received-message/index.js
@@ -1,5 +1,5 @@
 import Component from '@glimmer/component';
 
 export default class ReceivedMessage extends Component {
-    localTime = () => new Date().toLocaleTimeString();
+  localTime = () => new Date().toLocaleTimeString();
 }
